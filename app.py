1f54d95e0a8fad419b50541c5cf315abd3cf34d5
Kpêdido Godonou <rgodonou@africanschoolofeconomics.com>
2025-12-17 04:03:43 +0100

    Update comments

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 import streamlit as st
 
+# Page Title
 st.set_page_config(page_title="Onco Graph", page_icon="🕸️")
 
-visualize_table_page = st.Page(
-    "visualize_table.py",
-    title="Onco Graph Data Explorer",
-    icon=":material/table_chart:"
-)
-
+# Chat Page
 chat_page = st.Page(
     "chat.py",
     title="Onco-Graph Chat",
     icon=":material/smart_toy:"
 )
 
+# Data Explorer Page
+visualize_table_page = st.Page(
+    "visualize_table.py",
+    title="Onco Graph Data Explorer",
+    icon=":material/table_chart:"
+)
+
+# Setup page navigation 
 pg = st.navigation(
     {
         "Pages": [chat_page, visualize_table_page],
     }
 )
 
+# Run the app
 pg.run()
